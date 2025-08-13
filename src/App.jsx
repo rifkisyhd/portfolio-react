461dc22632197b01206f6e1ed310fe6cada6da3e
rifkisyhd <rifkisyahad81@gmail.com>
2025-08-13 22:59:37 +0700

    setting icon + nama tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -7,16 +7,34 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="bg-gray-100 text-gray-800">
-      <Navbar />
-      <Hero />
-      <About />
-      <Project />
-      <Contact />
-      <Footer />
-    </div>
-  );
+    useEffect(() => {
+        const originalTitle = document.title;
+        const handleVisibilityChange = () => {
+            if (document.hidden) {
+                document.title = "Jangan Lupa Kembali 😁";
+            } else {
+                document.title = originalTitle;
+            }
+        };
+        document.addEventListener("visibilitychange", handleVisibilityChange);
+        return () => {
+            document.removeEventListener(
+                "visibilitychange",
+                handleVisibilityChange,
+            );
+        };
+    }, []);
+
+    return (
+        <div className="bg-gray-100 text-gray-800">
+            <Navbar />
+            <Hero />
+            <About />
+            <Project />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
